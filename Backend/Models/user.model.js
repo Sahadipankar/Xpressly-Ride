ce174067fa9ae087eead2d83081b8f93e4e8725b
Sahadipankar <sahadepankar@gmail.com>
2025-04-26 09:31:49 +0530

    New files added

---
+++ Backend/Models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');   // Import Mongoose for MongoDB object modeling
+const bcrypt = require('bcryptjs');     // Import bcrypt for password hashing   
+const jwt = require('jsonwebtoken');    // Import jsonwebtoken for creating JWTs
+
+
+
+const userSchema = new mongoose.Schema({    // Define the user schema
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, "First name must be at least 3 characters long"]
+        },
+        lastname: {
+            type: String,
+            minlength: [3, "Last name must be at least 3 characters long"]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, "Email must be at least 5 characters long"],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,  // Exclude password from queries by default
+    },
+    socketId: {  // Optional field for storing socket ID
+        type: String,
+    },
+});
+
+
+userSchema.methods.generateAuthToken = function () {   // Method to generate JWT token
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);  // Sign the token with user ID and secret
+    return token;  // Return the generated token
+}
+
+userSchema.methods.comparePassword = async function (password) {  // Method to compare password with hashed password
+    return await bcrypt.compare(password, this.password);  
+}
+
+userSchema.statics.hashPassword = async function (password) {  // Static method to hash password
+    return await bcrypt.hash(password, 10);  // Hash the password with a salt rounds of 10
+}
+
+
+const userModel = mongoose.model('User', userSchema);  // Create a Mongoose model for the user schema
+
+
+module.exports = userModel;  // Export the user model for use in other files
--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,10 @@ dotenv.config(); // Load environment variables from .env file
 const express = require('express'); // Import Express framework
 const cors = require('cors'); // Import CORS middleware
 const app = express(); // Create an Express application
+const connectToDB = require('./DB/db'); // Import the database connection function
+
+
+connectToDB(); // Call the function to connect to the database
 
 app.use(cors()); // Use CORS middleware to enable Cross-Origin Resource Sharing
 
