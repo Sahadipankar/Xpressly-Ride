12f9c060ef8f45a36d9c3ca5eb08f043db337bf5
Sahadipankar <sahadepankar@gmail.com>
2025-06-20 02:34:19 +0530

    Add GSAP animations and remixicon integration to Home component; enhance form handling for pickup and destination inputs

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
 
 const Home = () => {
+
+    const [pickup, setPickup] = useState("");
+    const [destination, setDestination] = useState("");
+    const [panelOpen, setPanelOpen] = useState(false);
+    const panelRef = useRef(null);
+    const panelCloseRef = useRef(null);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
+
+    useGSAP(() => {
+        if (panelOpen) {
+            gsap.to(panelRef.current, {
+                height: '70%'
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 1,
+            })
+        } else {
+            gsap.to(panelRef.current, {
+                height: '0%'
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 0,
+            })
+        }
+    }, [panelOpen]);
+
     return (
         <div className='h-screen relative'>
             <img className='w-17 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
@@ -10,14 +43,38 @@ const Home = () => {
             </div>
 
             <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-                <div className='h-[30%] bg-white p-5'>
+                <div className='h-[30%] bg-white p-6 relative'>
+                    <h5 ref={panelCloseRef}
+                        onClick={() => setPanelOpen(false)}
+                        className='absolute opacity-0 right-6 top-6 text-2xl'>
+                        <i className="ri-arrow-down-wide-line"></i>
+                    </h5 >
                     <h4 className='text-2xl font-semibold'>Find a Trip</h4>
-                    <form>
-                        <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5' type="text" placeholder="Add a pick-up location" />
-                        <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3' type="text" placeholder="Enter your destination" />
+
+                    <form onSubmit={(e) => {
+                        submitHandler(e)
+                    }}>
+
+                        <div className="line absolute h-16 w-1 top-[45%] bg-gray-700 left-10 rounded-full"></div>
+                        <input
+                            onClick={() => setPanelOpen(true)}
+                            value={pickup}
+                            onChange={(e) => setPickup(e.target.value)}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5 focus:outline-none focus:ring-2 focus:ring-yellow-500'
+                            type="text"
+                            placeholder="Add a pick-up location"
+                        />
+                        <input
+                            onClick={() => setPanelOpen(true)}
+                            value={destination}
+                            onChange={(e) => setDestination(e.target.value)}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3 focus:outline-none focus:ring-2 focus:ring-yellow-400'
+                            type="text"
+                            placeholder="Enter your destination"
+                        />
                     </form>
                 </div>
-                <div className='bg-red-500 h-0'>
+                <div ref={panelRef} className='bg-red-500 h-0'>
 
                 </div>
 
